ad8b11695edc59d8f3e767abf4c0ff6067bf6723
Thomas <evers.thomas@gmail.com>
2020-03-25 13:41:05 +0100

    Create basic-page.php

---
+++ basic-page.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  ** use the following in your config.php to activate Markup Regions
+  ** $ config-> useMarkupRegions = true; - activate markup regions
+  ** $ config-> appendTemplateFile = '_main.php'; - add on every file a _main.php at the end
+*/
+
+
+
+?>
